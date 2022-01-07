b07ad76e917a90360bb1efa5eeddaecc844d577d
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-01-07 10:21:30 +0530

    C

--- src/main/java/com/th/ws/demo/EmployeeEndpoint.java
+++ src/main/java/com/th/ws/demo/EmployeeEndpoint.java
@@ -3,6 +3,13 @@ package com.th.ws.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import https.www_torryharris_com.soap_ws_demo.Employee;
+import https.www_torryharris_com.soap_ws_demo.GetEmployeeRequest;
+import https.www_torryharris_com.soap_ws_demo.GetEmployeeResponse;
 
 @Endpoint
 public class EmployeeEndpoint {
@@ -16,9 +23,17 @@ public class EmployeeEndpoint {
 
 	// similar to controller method
 
-//	public GetEmployeeResponse getEmployee() {
-	public Object getEmployee() {
-		return null;
+	@PayloadRoot(namespace = "https://www.torryharris.com/soap-ws-demo", localPart = "getEmployeeRequest")
+	@ResponsePayload
+	public GetEmployeeResponse getEmployee(@RequestPayload GetEmployeeRequest request) {
+		LOG.info("getEmployee " + request.getEmployeeId());
+		GetEmployeeResponse response = new GetEmployeeResponse();
+		Employee emp = new Employee(); // data will come from DB
+		emp.setEmployeeId(101);
+		emp.setFirstName("Sonu");
+		emp.setSalary(50000);
+		response.setEmployee(emp);
+		return response;
 	}
 
 }

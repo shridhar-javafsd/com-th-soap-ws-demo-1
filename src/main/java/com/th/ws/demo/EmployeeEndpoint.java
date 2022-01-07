a4d71277af2f08e2fc07b2d87ae88dd08dd09dae
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-01-07 09:56:42 +0530

    Update EmployeeEndpoint.java

--- src/main/java/com/th/ws/demo/EmployeeEndpoint.java
+++ src/main/java/com/th/ws/demo/EmployeeEndpoint.java
@@ -1,5 +1,24 @@
 package com.th.ws.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+
+@Endpoint
 public class EmployeeEndpoint {
+
+	Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+	public EmployeeEndpoint() {
+		super();
+		LOG.info("EmployeeEmdPoint");
+	}
+
+	// similar to controller method
+
+//	public GetEmployeeResponse getEmployee() {
+	public Object getEmployee() {
+		return null;
+	}
 
 }

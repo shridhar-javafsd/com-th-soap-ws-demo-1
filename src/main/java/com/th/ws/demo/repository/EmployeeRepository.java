f752abd682958382902323a9b199e29f9c083a46
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-01-10 10:28:39 +0530

    Update EmployeeRepository.java

--- src/main/java/com/th/ws/demo/repository/EmployeeRepository.java
+++ src/main/java/com/th/ws/demo/repository/EmployeeRepository.java
@@ -7,3 +7,10 @@ import com.th.ws.demo.model.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
+
+
+//SELECT * FROM employee_table;
+//
+//insert into employee_table values (101, 'Sonu', 50000);
+//insert into employee_table values (102, 'Monu', 58000);
+//insert into employee_table values (103, 'Tonu', 54000);

ced76871b1a20a6a301216bb754675d6a14acae3
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-01-11 22:28:24 +0530

    C

--- src/main/java/com/th/ws/demo/model/Employee.java
+++ src/main/java/com/th/ws/demo/model/Employee.java
@@ -1,6 +1,8 @@
 package com.th.ws.demo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,8 +11,11 @@ import javax.persistence.Table;
 public class Employee {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int employeeId;
+
 	private String firstName;
+
 	private double salary;
 
 	public Employee() {
